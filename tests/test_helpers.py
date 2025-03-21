73449110549eae8bba9e73c4199d28a1b3fdaf66
iarp <iarp.opensource@gmail.com>
2025-03-21 14:39:39 -0400

    tests(helpers): tests for video, statistics, file, extrafile, channel helpers

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,10 +1,27 @@
 import datetime
 import io
+from unittest.mock import patch
 
 from django.test import TestCase
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
-from vidar import models
-from vidar.helpers import json_safe_kwargs, celery_helpers
+from vidar import models, app_settings
+from vidar.helpers import (
+    channel_helpers,
+    json_safe_kwargs,
+    extrafile_helpers,
+    celery_helpers,
+    video_helpers,
+    statistics_helpers,
+    file_helpers,
+)
+
+def date_to_aware_date(value):
+    y, m, d = value.split('-')
+    y, m, d = int(y), int(m), int(d)
+
+    return timezone.make_aware(timezone.datetime(y, m, d))
 
 
 class GeneralHelpersTests(TestCase):
@@ -46,3 +63,354 @@ class CeleryHelpersTests(TestCase):
 
         celery_helpers.object_lock_release(video)
         self.assertFalse(celery_helpers.is_object_locked(video))
+
+
+class VideoHelpersTests(TestCase):
+    def test_get_video_upload_to_directory_without_channel_with_upload_date(self):
+        video = models.Video.objects.create(title="test video 1", upload_date=date_to_aware_date('2023-01-01'))
+        output = video_helpers.get_video_upload_to_directory(instance=video)
+        self.assertEqual('public/2023', str(output))
+
+    def test_get_video_upload_to_directory_without_channel_nor_upload_date(self):
+        video = models.Video.objects.create(title="test video 1")
+        output = video_helpers.get_video_upload_to_directory(instance=video)
+        self.assertEqual(f'public/{timezone.now().year}', str(output))
+
+    def test_get_video_upload_to_directory_with_channel_year_sep_with_upload_date_and_vids_in_sep_dir(self):
+        channel = models.Channel.objects.create(
+            name="Test Channel",
+            store_videos_by_year_separation=True,
+            store_videos_in_separate_directories=True,
+        )
+        video = models.Video.objects.create(
+            channel=channel,
+            provider_object_id="video-id-1",
+            title="test video 1",
+            upload_date=date_to_aware_date('2023-01-01')
+        )
+        output = video_helpers.get_video_upload_to_directory(instance=video)
+        self.assertEqual('Test Channel/2023/2023-01-01 - test video 1 [video-id-1]', str(output))
+
+    def test_get_video_upload_to_directory_with_channel_year_sep_without_upload_date_and_vids_in_sep_dir(self):
+        channel = models.Channel.objects.create(
+            name="Test Channel",
+            store_videos_by_year_separation=True,
+            store_videos_in_separate_directories=True,
+        )
+        video = models.Video.objects.create(
+            channel=channel,
+            provider_object_id="video-id-1",
+            title="test video 1",
+        )
+        output = video_helpers.get_video_upload_to_directory(instance=video)
+        self.assertEqual(f'Test Channel/{timezone.now().year}/- test video 1 [video-id-1]', str(output))
+
+    def test_get_video_upload_to_directory_with_channel_not_year_sep_with_upload_date_and_vids_in_sep_dir(self):
+        channel = models.Channel.objects.create(
+            name="Test Channel",
+            store_videos_by_year_separation=False,
+            store_videos_in_separate_directories=True,
+        )
+        video = models.Video.objects.create(
+            channel=channel,
+            provider_object_id="video-id-1",
+            title="test video 1",
+            upload_date=date_to_aware_date('2023-01-01')
+        )
+        output = video_helpers.get_video_upload_to_directory(instance=video)
+        self.assertEqual('Test Channel/2023-01-01 - test video 1 [video-id-1]', str(output))
+
+    def test_get_video_upload_to_directory_with_channel_not_year_sep_with_upload_date_and_not_vids_in_sep_dir(self):
+        channel = models.Channel.objects.create(
+            name="Test Channel",
+            store_videos_by_year_separation=False,
+            store_videos_in_separate_directories=False,
+        )
+        video = models.Video.objects.create(
+            channel=channel,
+            provider_object_id="video-id-1",
+            title="test video 1",
+            upload_date=date_to_aware_date('2023-01-01')
+        )
+        output = video_helpers.get_video_upload_to_directory(instance=video)
+        self.assertEqual('Test Channel', str(output))
+
+    def test_get_video_upload_to_directory_with_channel_year_sep_with_upload_date_and_not_vids_in_sep_dir(self):
+        channel = models.Channel.objects.create(
+            name="Test Channel",
+            store_videos_by_year_separation=True,
+            store_videos_in_separate_directories=False,
+        )
+        video = models.Video.objects.create(
+            channel=channel,
+            provider_object_id="video-id-1",
+            title="test video 1",
+            upload_date=date_to_aware_date('2023-01-01')
+        )
+        output = video_helpers.get_video_upload_to_directory(instance=video)
+        self.assertEqual('Test Channel/2023', str(output))
+
+    def test_get_video_upload_to_directory_with_channel_year_sep_without_upload_date_and_not_vids_in_sep_dir(self):
+        channel = models.Channel.objects.create(
+            name="Test Channel",
+            store_videos_by_year_separation=True,
+            store_videos_in_separate_directories=False,
+        )
+        video = models.Video.objects.create(
+            channel=channel,
+            provider_object_id="video-id-1",
+            title="test video 1",
+        )
+        output = video_helpers.get_video_upload_to_directory(instance=video)
+        self.assertEqual(f'Test Channel/{timezone.now().year}', str(output))
+
+    def test_video_upload_to_side_by_side(self):
+        video = models.Video.objects.create(title="Test Video 1")
+        output = video_helpers.video_upload_to_side_by_side(instance=video, filename="test.mp4")
+        self.assertEqual("public/2025/test.mp4", str(output))
+
+        channel = models.Channel.objects.create(name="test channel")
+        video = models.Video.objects.create(
+            title="Test Video 1", channel=channel, provider_object_id="test-id",
+            upload_date=date_to_aware_date('2025-01-01')
+        )
+        output = video_helpers.video_upload_to_side_by_side(instance=video, filename="test.mp4")
+        self.assertEqual("test channel/2025/2025-01-01 - Test Video 1 [test-id]/test.mp4", str(output))
+
+    def test_upload_to_file_and_infojson_and_thumbnail(self):
+        video = models.Video.objects.create(title="Test Video 1", upload_date=date_to_aware_date('2025-01-01'))
+
+        output = video_helpers.upload_to_file(instance=video, filename="test.mp4")
+        self.assertEqual("public/2025/test.mp4", str(output))
+        output = video_helpers.upload_to_infojson(instance=video, filename="test.info.json")
+        self.assertEqual("public/2025/test.info.json", str(output))
+        output = video_helpers.upload_to_thumbnail(instance=video, filename="test.jpg")
+        self.assertEqual("public/2025/test.jpg", str(output))
+
+        channel = models.Channel.objects.create(name="test channel")
+        video = models.Video.objects.create(
+            title="Test Video 1",
+            channel=channel,
+            provider_object_id="test-id",
+            upload_date=date_to_aware_date('2025-01-01'))
+
+        output = video_helpers.upload_to_file(instance=video, filename="test.mp4")
+        self.assertEqual("test channel/2025/2025-01-01 - Test Video 1 [test-id]/test.mp4", str(output))
+
+        output = video_helpers.upload_to_infojson(instance=video, filename="test.info.json")
+        self.assertEqual("test channel/2025/2025-01-01 - Test Video 1 [test-id]/test.info.json", str(output))
+
+        output = video_helpers.upload_to_thumbnail(instance=video, filename="test.jpg")
+        self.assertEqual("test channel/2025/2025-01-01 - Test Video 1 [test-id]/test.jpg", str(output))
+
+    def test_default_quality(self):
+        self.assertEqual(app_settings.DEFAULT_QUALITY, video_helpers.default_quality())
+
+    def test_upload_to_audio_without_channel_with_upload_date(self):
+        video = models.Video.objects.create(title="test video 1", upload_date=date_to_aware_date('2023-01-01'))
+        output = video_helpers.upload_to_audio(instance=video, filename="test.mp3")
+        self.assertEqual('public/audio/2023/test.mp3', str(output))
+
+    def test_upload_to_audio_without_channel_nor_upload_date(self):
+        video = models.Video.objects.create(title="test video 1")
+        output = video_helpers.upload_to_audio(instance=video, filename="test.mp3")
+        self.assertEqual(f'public/audio/{timezone.now().year}/test.mp3', str(output))
+
+    def test_upload_to_audio_with_channel_with_upload_date(self):
+        channel = models.Channel.objects.create(name="Test Channel")
+        video = models.Video.objects.create(
+            channel=channel,
+            title="test video 1",
+            upload_date=date_to_aware_date('2023-01-01'),
+        )
+        output = video_helpers.upload_to_audio(instance=video, filename="test.mp3")
+        self.assertEqual('Test Channel/audio/2023/test.mp3', str(output))
+
+    def test_upload_to_audio_with_channel_nor_upload_date(self):
+        channel = models.Channel.objects.create(name="Test Channel")
+        video = models.Video.objects.create(title="test video 1", channel=channel)
+        output = video_helpers.upload_to_audio(instance=video, filename="test.mp3")
+        self.assertEqual(f'Test Channel/audio/{timezone.now().year}/test.mp3', str(output))
+
+
+class ExtraFileHelpersTests(TestCase):
+
+    def test_extrafile_file_upload_to_without_channel_with_upload_date(self):
+        video = models.Video.objects.create(title="test video 1", upload_date=date_to_aware_date('2023-01-01'))
+        ef = models.ExtraFile.objects.create(video=video)
+        output = extrafile_helpers.extrafile_file_upload_to(instance=ef, filename="test.mp3")
+        self.assertEqual('public/2023/test.mp3', str(output))
+
+    def test_extrafile_file_upload_to_without_channel_nor_upload_date(self):
+        video = models.Video.objects.create(title="test video 1")
+        ef = models.ExtraFile.objects.create(video=video)
+        output = extrafile_helpers.extrafile_file_upload_to(instance=ef, filename="test.mp3")
+        self.assertEqual(f'public/{timezone.now().year}/test.mp3', str(output))
+
+    def test_extrafile_file_upload_to_with_channel_with_upload_date(self):
+        channel = models.Channel.objects.create(name="Test Channel")
+        video = models.Video.objects.create(
+            channel=channel,
+            title="test video 1",
+            upload_date=date_to_aware_date('2023-01-01'),
+        )
+        ef = models.ExtraFile.objects.create(video=video)
+        output = extrafile_helpers.extrafile_file_upload_to(instance=ef, filename="test.mp3")
+        self.assertEqual('Test Channel/2023/2023-01-01 - test video 1 []/test.mp3', str(output))
+
+    def test_extrafile_file_upload_to_with_channel_nor_upload_date(self):
+        channel = models.Channel.objects.create(name="Test Channel")
+        video = models.Video.objects.create(title="test video 1", channel=channel)
+        ef = models.ExtraFile.objects.create(video=video)
+        output = extrafile_helpers.extrafile_file_upload_to(instance=ef, filename="test.mp3")
+        self.assertEqual(f'Test Channel/{timezone.now().year}/- test video 1 []/test.mp3', str(output))
+
+
+class StatisticsHelpersTests(TestCase):
+
+    def test_channel_most_common_date_weekday_returns_zero_through_six_only(self):
+        channel = models.Channel.objects.create(
+            provider_object_id='tests',
+        )
+        channel.videos.create(provider_object_id='tests', upload_date='2024-09-01')
+
+        self.assertEqual(0, statistics_helpers.most_common_date_weekday(queryset=channel.videos))
+
+        models.Video.objects.all().update(upload_date='2024-09-02')
+        self.assertEqual(1, statistics_helpers.most_common_date_weekday(queryset=channel.videos))
+
+        models.Video.objects.all().update(upload_date='2024-09-03')
+        self.assertEqual(2, statistics_helpers.most_common_date_weekday(queryset=channel.videos))
+
+        models.Video.objects.all().update(upload_date='2024-09-04')
+        self.assertEqual(3, statistics_helpers.most_common_date_weekday(queryset=channel.videos))
+
+        models.Video.objects.all().update(upload_date='2024-09-05')
+        self.assertEqual(4, statistics_helpers.most_common_date_weekday(queryset=channel.videos))
+
+        models.Video.objects.all().update(upload_date='2024-09-06')
+        self.assertEqual(5, statistics_helpers.most_common_date_weekday(queryset=channel.videos))
+
+        models.Video.objects.all().update(upload_date='2024-09-07')
+        self.assertEqual(6, statistics_helpers.most_common_date_weekday(queryset=channel.videos))
+
+        models.Video.objects.all().update(upload_date='2024-09-08')
+        self.assertEqual(0, statistics_helpers.most_common_date_weekday(queryset=channel.videos))
+
+    def test_channel_most_common_date_day_of_month(self):
+        channel = models.Channel.objects.create(
+            provider_object_id='tests',
+        )
+        channel.videos.create(provider_object_id='tests', upload_date='2024-09-19')
+
+        self.assertEqual(19, statistics_helpers.most_common_date_day_of_month(queryset=channel.videos))
+
+    def test_channel_most_common_date_day_of_month_with_multiple_videos_on_separate_days(self):
+        channel = models.Channel.objects.create(
+            provider_object_id='tests',
+        )
+        channel.videos.create(provider_object_id='tests', upload_date='2024-09-19')
+        channel.videos.create(provider_object_id='tests', upload_date='2024-08-18')
+
+        self.assertEqual(19, statistics_helpers.most_common_date_day_of_month(queryset=channel.videos))
+
+    def test_channel_most_common_date_day_of_month_with_multiple_videos_on_same_days(self):
+        channel = models.Channel.objects.create(
+            provider_object_id='tests',
+        )
+        channel.videos.create(provider_object_id='tests', upload_date='2024-09-19')
+        channel.videos.create(provider_object_id='tests', upload_date='2024-08-18')
+        channel.videos.create(provider_object_id='tests', upload_date='2024-08-19')
+
+        self.assertEqual(19, statistics_helpers.most_common_date_day_of_month(queryset=channel.videos))
+
+    def test_channel_most_common_date_day_of_month_with_31st_as_most_common_returns_30(self):
+        channel = models.Channel.objects.create(
+            provider_object_id='tests',
+        )
+        channel.videos.create(provider_object_id='tests', upload_date='2024-08-31')
+        channel.videos.create(provider_object_id='tests', upload_date='2024-08-18')
+        channel.videos.create(provider_object_id='tests', upload_date='2024-08-31')
+
+        self.assertEqual(30, statistics_helpers.most_common_date_day_of_month(queryset=channel.videos))
+
+    def test_channel_most_common_date_week_of_year(self):
+        channel = models.Channel.objects.create(provider_object_id='tests')
+        channel.videos.create(provider_object_id='tests1', upload_date='2024-09-19')
+        channel.videos.create(provider_object_id='tests2', upload_date='2024-09-20')
+        channel.videos.create(provider_object_id='tests3', upload_date='2024-12-20')
+
+        self.assertEqual(38, statistics_helpers.most_common_date_week_of_year(queryset=channel.videos))
+
+
+class FileHelpersTests(TestCase):
+
+    def test_is_field_using_local_storage(self):
+        video = models.Video.objects.create()
+        self.assertTrue(file_helpers.is_field_using_local_storage(video.file))
+
+    def test_is_field_using_local_storage_custom(self):
+        video = models.Video.objects.create()
+        class CustomStorage:
+            pass
+        video.file.storage = CustomStorage()
+        self.assertFalse(file_helpers.is_field_using_local_storage(video.file))
+
+    def test_can_file_be_moved(self):
+        video = models.Video.objects.create()
+        self.assertTrue(file_helpers.can_file_be_moved(video.file))
+
+    def test_can_file_be_moved_other_storage(self):
+        video = models.Video.objects.create()
+        class CustomStorage:
+            pass
+        video.file.storage = CustomStorage()
+        self.assertFalse(file_helpers.can_file_be_moved(video.file))
+
+    def test_ensure_file_is_local(self):
+        video = models.Video.objects.create(file="test.mp4")
+        local_filepath, was_remote = file_helpers.ensure_file_is_local(video.file)
+        self.assertFalse(was_remote)
+
+    @patch("os.fdopen")
+    @patch("tempfile.mkstemp")
+    def test_ensure_file_is_local_other_storage(self, mock_temp, mock_fdopen):
+        mock_temp.return_value = (1, "path value")
+        video = models.Video.objects.create(file="test.mp4")
+        class CustomStorage:
+            def open(self, *args, **kwargs):
+                return self
+            def read(self, *args, **kwargs):
+                return ""
+            def close(self, *args, **kwargs):
+                return ""
+        video.file.storage = CustomStorage()
+        local_filepath, was_remote = file_helpers.ensure_file_is_local(video.file)
+        self.assertTrue(was_remote)
+
+        mock_fdopen.assert_called_once()
+        mock_temp.assert_called_once()
+
+
+class ChannelHelpersTests(TestCase):
+    def test_watched_percentage_minimum(self):
+        with self.assertRaises(ValidationError):
+            channel_helpers.watched_percentage_minimum(0)
+        self.assertIsNone(channel_helpers.watched_percentage_minimum(1))
+
+    def test_watched_percentage_maximum(self):
+        with self.assertRaises(ValidationError):
+            channel_helpers.watched_percentage_maximum(101)
+        self.assertIsNone(channel_helpers.watched_percentage_maximum(100))
+
+    def test_upload_to_fields(self):
+        channel = models.Channel.objects.create(name="Test Channel")
+
+        output = channel_helpers.upload_to_banner(instance=channel, filename="banner.jpg")
+        self.assertEqual("Test Channel/banner.jpg", str(output))
+
+        output = channel_helpers.upload_to_thumbnail(instance=channel, filename="thumbnail.jpg")
+        self.assertEqual("Test Channel/thumbnail.jpg", str(output))
+
+        output = channel_helpers.upload_to_tvart(instance=channel, filename="tvart.jpg")
+        self.assertEqual("Test Channel/tvart.jpg", str(output))
