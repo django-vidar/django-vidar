e0fa2218c21879dad194f754381ddd548e2017f3
iarp <iarp.opensource@gmail.com>
2025-03-23 13:31:37 -0400

    tests(services): add tests for image_services

--- tests/test_services.py
+++ tests/test_services.py
@@ -3,7 +3,7 @@ import json
 import logging
 import pathlib
 
-from unittest.mock import patch, call
+from unittest.mock import patch, call, MagicMock
 
 from django.conf import settings
 from django.test import TestCase, SimpleTestCase, override_settings
@@ -18,6 +18,7 @@ from vidar.services import (
     video_services,
     crontab_services,
     playlist_services,
+    image_services,
     redis_services
 )
 from vidar.helpers import video_helpers
@@ -2451,3 +2452,81 @@ class RedisServicesLiveTests(TestCase):
             },
         ]
         self.assertCountEqual(expected, output)
+
+
+class ImageServicesTests(SimpleTestCase):
+
+    WEBP_IMAGE_CONTENTS = b'RIFF\x06\x01\x00\x00WEBPVP8X\n\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00' \
+                          b'VP8L\n\x00\x00\x00/\x00\x00\x00\x00E\xff#\xfa\x1fEXIF\xd6\x00\x00\x00II*\x00\x08\x00' \
+                          b'\x00\x00\x06\x00\x12\x01\x03\x00\x01\x00\x00\x00\x01\x00\x00\x00\x1a\x01\x05\x00\x01' \
+                          b'\x00\x00\x00V\x00\x00\x00\x1b\x01\x05\x00\x01\x00\x00\x00^\x00\x00\x00(\x01\x03\x00' \
+                          b'\x01\x00\x00\x00\x02\x00\x00\x001\x01\x02\x00\x10\x00\x00\x00f\x00\x00\x00i\x87\x04' \
+                          b'\x00\x01\x00\x00\x00v\x00\x00\x00\x00\x00\x00\x00`\x00\x00\x00\x01\x00\x00\x00`\x00' \
+                          b'\x00\x00\x01\x00\x00\x00Paint.NET 5.1.4\x00\x05\x00\x00\x90\x07\x00\x04\x00\x00\x000230' \
+                          b'\x01\xa0\x03\x00\x01\x00\x00\x00\x01\x00\x00\x00\x02\xa0\x04\x00\x01\x00\x00\x00\x01' \
+                          b'\x00\x00\x00\x03\xa0\x04\x00\x01\x00\x00\x00\x01\x00\x00\x00\x05\xa0\x04\x00\x01\x00' \
+                          b'\x00\x00\xb8\x00\x00\x00\x00\x00\x00\x00\x02\x00\x01\x00\x02\x00\x04\x00\x00\x00R98' \
+                          b'\x00\x02\x00\x07\x00\x04\x00\x00\x000100\x00\x00\x00\x00'
+
+    JPG_IMAGE_CONTENTS = b'\xff\xd8\xff\xe0\x00\x10JFIF\x00\x01\x01\x00\x00\x01\x00\x01\x00\x00\xff\xdb\x00C\x00' \
+                         b'\x08\x06\x06\x07\x06\x05\x08\x07\x07\x07\t\t\x08\n\x0c\x14\r\x0c\x0b\x0b\x0c\x19\x12' \
+                         b'\x13\x0f\x14\x1d\x1a\x1f\x1e\x1d\x1a\x1c\x1c $.\' ",#\x1c\x1c(7),01444\x1f\'9=82<.342' \
+                         b'\xff\xdb\x00C\x01\t\t\t\x0c\x0b\x0c\x18\r\r\x182!\x1c!22222222222222222222222222222' \
+                         b'222222222222222222222\xff\xc0\x00\x11\x08\x00\x01\x00\x01\x03\x01"\x00\x02\x11\x01' \
+                         b'\x03\x11\x01\xff\xc4\x00\x1f\x00\x00\x01\x05\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00' \
+                         b'\x00\x00\x00\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\xff\xc4\x00\xb5\x10\x00\x02' \
+                         b'\x01\x03\x03\x02\x04\x03\x05\x05\x04\x04\x00\x00\x01}\x01\x02\x03\x00\x04\x11\x05\x12!1A' \
+                         b'\x06\x13Qa\x07"q\x142\x81\x91\xa1\x08#B\xb1\xc1\x15R\xd1\xf0$3br\x82\t\n\x16\x17\x18\x19' \
+                         b'\x1a%&\'()*456789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz\x83\x84\x85\x86\x87\x88\x89\x8a\x92' \
+                         b'\x93\x94\x95\x96\x97\x98\x99\x9a\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xb2\xb3\xb4\xb5\xb6' \
+                         b'\xb7\xb8\xb9\xba\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9' \
+                         b'\xda\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa' \
+                         b'\xff\xc4\x00\x1f\x01\x00\x03\x01\x01\x01\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\x00' \
+                         b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\xff\xc4\x00\xb5\x11\x00\x02\x01\x02\x04' \
+                         b'\x04\x03\x04\x07\x05\x04\x04\x00\x01\x02w\x00\x01\x02\x03\x11\x04\x05!1\x06\x12AQ\x07aq' \
+                         b'\x13"2\x81\x08\x14B\x91\xa1\xb1\xc1\t#3R\xf0\x15br\xd1\n\x16$4\xe1%\xf1\x17\x18\x19' \
+                         b'\x1a&\'()*56789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz\x82\x83\x84\x85\x86\x87\x88\x89\x8a' \
+                         b'\x92\x93\x94\x95\x96\x97\x98\x99\x9a\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xb2\xb3\xb4' \
+                         b'\xb5\xb6\xb7\xb8\xb9\xba\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xd2\xd3\xd4\xd5\xd6\xd7' \
+                         b'\xd8\xd9\xda\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa' \
+                         b'\xff\xda\x00\x0c\x03\x01\x00\x02\x11\x03\x11\x00?\x00\xf7\xfa(\xa2\x80?\xff\xd9'
+
+    def test_convert_image_to_jpg_in_memory(self):
+        output = image_services._convert_image_to_jpg_in_memory(image_bytes=self.WEBP_IMAGE_CONTENTS)
+        self.assertEqual(self.JPG_IMAGE_CONTENTS, output)
+
+    @patch("requests.get")
+    def test_download_and_convert_to_jpg(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.content = self.WEBP_IMAGE_CONTENTS
+        mock_get.return_value = mock_response
+
+        url = "https://www.domain.com/image.webp"
+        output, file_ext = image_services.download_and_convert_to_jpg(url)
+
+        self.assertEqual(file_ext, "jpg")
+        self.assertEqual(self.JPG_IMAGE_CONTENTS, output)
+
+    @patch("requests.get")
+    def test_download_and_convert_to_jpg_failure_returns_original_data(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.content = "bad data"
+        mock_get.return_value = mock_response
+
+        url = "https://www.domain.com/image.webp"
+        output, file_ext = image_services.download_and_convert_to_jpg(url)
+
+        self.assertEqual(file_ext, "webp")
+        self.assertEqual("bad data", output)
+
+    @patch("requests.get")
+    def test_download_and_convert_to_jpg_url_missing_ext_returns_jpg(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.content = self.WEBP_IMAGE_CONTENTS
+        mock_get.return_value = mock_response
+
+        url = "https://www.domain.com/image"
+        output, file_ext = image_services.download_and_convert_to_jpg(url)
+
+        self.assertEqual(file_ext, "jpg")
+        self.assertEqual(self.WEBP_IMAGE_CONTENTS, output)
